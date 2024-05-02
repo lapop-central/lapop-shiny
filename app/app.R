9da18f7ff73efdc3d5f041d75b75d685ad5825df
pluto2493 <luke.plutowski@gmail.com>
2024-05-01 20:00:08 -0500

    update with stata code

--- app/app.R
+++ app/app.R
@@ -1,4 +1,7 @@
 library(shiny)
+library(lapop)
+library(RStata)
+setwd("C:/Users/plutowl/Documents/GitHub/lapop-shiny")
 
 
 # Define UI for miles per gallon app ----
@@ -44,8 +47,6 @@ ui <- fluidPage(
 # doesn't rely on any user inputs, we can do this once at startup
 # and then use the value throughout the lifetime of the app
 
-library(lapop)
-setwd("C:/Users/plutowl/Documents/GitHub/lapop-shiny")
 # ym23 <- readstata13::read.dta13("Shiny data/Merge 2023 LAPOP AmericasBarometer (v1.0s).dta")
 # select <- c("pais", "wave", "ing4", "vic1ext", "edre", "q1tc_r", "wealth", "edad", "weight1500")
 # ym23s <- ym23[select]
@@ -54,9 +55,14 @@ setwd("C:/Users/plutowl/Documents/GitHub/lapop-shiny")
 # select21 <- c("pais", "wave", "ing4", "vic1ext", "edre", "q1tc_r", "wealth", "edad", "weight1500")
 # ym21s <- ym21[select]
 
-gm <- readstata13::read.dta13("Shiny data/gm2123.dta", convert.factors = FALSE)
-select <- c("pais", "wave", "ing4", "vic1ext", "wealth", "edad")
-gms <- gm[select]
+# gm <- readstata13::read.dta13("C:/Users/plutowl/Desktop/Shiny data/gm2123.dta", convert.factors = FALSE)
+# select <- c("pais", "wave", "ing4", "vic1ext", "wealth", "edad")
+# gms <- gm[select]
+
+# gm <- readstata13::read.dta13("C:/Users/plutowl/Box/LAPOP Shared/2_Projects/2023 AB/BHS/Data Processing/BHS merge 2014-2023 LAPOP AmericasBarometer (v1.0s).dta", convert.factors = FALSE)
+
+num_max <- 2
+num <- paste(1:num_max, collapse = " ")
 
 # Define server logic to plot various variables against mpg ----
 server <- function(input, output) {
@@ -67,6 +73,11 @@ server <- function(input, output) {
   formulaText <- reactive({
     paste(input$variable)
   })
+  
+  
+
+  
+
   
   outcome <- reactive({
     # if(input$weighted == TRUE){
@@ -86,28 +97,43 @@ server <- function(input, output) {
   
 
   
+  # output$hist <- renderPlot({
+  #   # the_wt = ifelse(input$weighted, gms$weight1500, 1)
+  #   # x = outcome() * the_wt
+  #   # var = input$variable
+  #   hist_df <- as.data.frame(prop.table(table(gms[[outcome()]]))*100)
+  #   names(hist_df) <- c("cat", "prop")
+  #   hist_df$proplabel <- paste(round(hist_df$prop), "%", sep = "")
+  #   lapop_hist(hist_df)
+  # })
+  
   output$hist <- renderPlot({
-    # the_wt = ifelse(input$weighted, gms$weight1500, 1)
-    # x = outcome() * the_wt
-    # var = input$variable
-    hist_df <- as.data.frame(prop.table(table(gms[[outcome()]]))*100)
-    names(hist_df) <- c("cat", "prop")
-    hist_df$proplabel <- paste(round(hist_df$prop), "%", sep = "")
+    stata_cmd2 <- paste('use "C:\\Users\\plutowl\\Box\\LAPOP Shared\\2_Projects\\2023 AB\\BHS\\Data Processing\\BHS merge 2014-2023 LAPOP AmericasBarometer (v1.0s).dta", clear\n', 
+                        "lpr_hist ", outcome(), ', filesave("hist.csv", replace) \n',
+                        'lpr_ts ', outcome(), ' wave, num(', num, ') filesave("ts.csv", replace)')
+    stata(stata_cmd2)
+    hist_df <- read.csv("hist.csv")
     lapop_hist(hist_df)
   })
   
-  
   output$ts <- renderPlot({
-    dta = gms %>%
-      group_by(wave) %>%
-      summarise_at(vars(input$variable), list(name = mean), na.rm = TRUE)
-    dta$wave = as.character(dta$wave)
-    dta$proplabel = paste(as.character(round(dta$name), 0))
-    dta$lb = dta$name - 1
-    dta$ub = dta$name + 1
-    names(dta)[2] = "prop"
-    lapop_ts(dta)
+    ts_df <- read.csv("ts.csv")
+    lapop_ts(ts_df)
   })
+  
+  
+  
+  # output$ts <- renderPlot({
+  #   dta = gms %>%
+  #     group_by(wave) %>%
+  #     summarise_at(vars(input$variable), list(name = mean), na.rm = TRUE)
+  #   dta$wave = as.character(dta$wave)
+  #   dta$proplabel = paste(as.character(round(dta$name), 0))
+  #   dta$lb = dta$name - 1
+  #   dta$ub = dta$name + 1
+  #   names(dta)[2] = "prop"
+  #   lapop_ts(dta)
+  # })
   
 }
 
